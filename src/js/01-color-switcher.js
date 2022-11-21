a8429dd6347f8d5524aeca4335ad59f5eb3211bb
Vlad Pazyna <vladpazyna@gmail.com>
2022-11-22 00:21:28 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let isActive = false;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onStart = () => {
+  isActive = true;
+  if (isActive) {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+  }
+
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+};
+
+const onStop = () => {
+  isActive = false;
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(timerId);
+};
+
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
